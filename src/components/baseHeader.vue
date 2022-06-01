5b0d292a57479042bf25726fff1aa6d5fedc47e8
Evanflow89 <ivanbaragone@gmail.com>
2022-06-01 16:33:51 +0200

    Added logo on header

---
+++ src/components/baseHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <div class="imgLogo">
+      <img src="../assets/img/dc-logo.png" alt="dc Logo" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "baseHeader",
+};
+</script>
+
+<style lang="scss">
+header {
+  background-color: white;
+}
+</style>
