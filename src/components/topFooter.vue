6d8014bceff374fd5dacdbb6183145119106cc24
Evanflow89 <ivanbaragone@gmail.com>
2022-06-01 18:04:28 +0200

    Top Footer Complete

--- src/components/topFooter.vue
+++ src/components/topFooter.vue
@@ -10,6 +10,42 @@
         </div>
         <div class="card_description">Digital Comics</div>
       </div>
+      <div class="card">
+        <div class="img_card">
+          <img
+            src="../assets/img/buy-comics-merchandise.png"
+            alt="Dc Merchandise"
+          />
+        </div>
+        <div class="card_description">Dc Merchandise</div>
+      </div>
+      <div class="card">
+        <div class="img_card">
+          <img
+            src="../assets/img/buy-comics-subscriptions.png"
+            alt="Dc Subscriptions"
+          />
+        </div>
+        <div class="card_description">Subscriptions</div>
+      </div>
+      <div class="card">
+        <div class="img_card">
+          <img
+            src="../assets/img/buy-comics-shop-locator.png"
+            alt="Dc Shop Locator"
+          />
+        </div>
+        <div class="card_description">Comic Shop Locator</div>
+      </div>
+      <div class="card">
+        <div class="img_card">
+          <img
+            src="../assets/img/buy-dc-power-visa.svg"
+            alt="Dc Buy Power Visa"
+          />
+        </div>
+        <div class="card_description">Dc Power Visa</div>
+      </div>
     </div>
   </footer>
 </template>
@@ -24,6 +60,9 @@ footer {
   background-color: var(--secondary-color);
   & .container {
     display: flex;
+    justify-content: space-around;
+    color: white;
+    text-transform: uppercase;
   }
   .card {
     display: flex;
