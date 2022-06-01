e6df31655eec18348df6e33f2d5351d0088f04eb
Evanflow89 <ivanbaragone@gmail.com>
2022-06-01 16:55:08 +0200

    Added Vue Object

--- src/components/baseHeader.vue
+++ src/components/baseHeader.vue
@@ -45,11 +45,68 @@
 <script>
 export default {
   name: "baseHeader",
+  data() {
+    return {
+      links: [
+        {
+          text: "Characters",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Comics",
+          url: "#",
+          current: true,
+        },
+        {
+          text: "Movies",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Tv",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Games",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Collectibles",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Videos",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Fans",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "News",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Shop",
+          url: "#",
+          current: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss">
 header {
+  padding: 1.25rem 0;
   background-color: white;
   display: flex;
   flex-direction: row;
