4b50dbcc8a1e85cd56d2a3de25a55dad48ea84bd
Evanflow89 <ivanbaragone@gmail.com>
2022-06-02 11:16:27 +0200

    Started middleFooter

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <baseHeader />
     <baseMain />
     <topFooter />
+    <middleFooter />
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import baseHeader from "./components/baseHeader.vue";
 import baseMain from "./components/baseMain.vue";
 import topFooter from "./components/topFooter.vue";
+import middleFooter from "./components/middleFooter.vue";
 
 export default {
   name: "App",
@@ -17,6 +19,7 @@ export default {
     baseHeader,
     baseMain,
     topFooter,
+    middleFooter,
   },
 };
 </script>
---
+++ src/components/middleFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="middleFooter">
+    <div class="container">
+      <div class="col-left"></div>
+      <div class="col-left"></div>
+      <div class="col-left"></div>
+      <div class="col-right"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.middleFooter {
+  background-image: url("../assets/img/footer-bg.jpg");
+  &.container {
+  }
+  .col-left {
+    width: 20%;
+  }
+  .col-right {
+    width: 40%;
+  }
+}
+</style>
