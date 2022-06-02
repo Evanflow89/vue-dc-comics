b1e1d50039a1002d4a2640565fce600586a28ed7
Evanflow89 <ivanbaragone@gmail.com>
2022-06-02 11:52:27 +0200

    Added Ul list

--- src/components/middleFooter.vue
+++ src/components/middleFooter.vue
@@ -1,9 +1,57 @@
 <template>
   <div class="middleFooter">
     <div class="container">
-      <div class="col-left"></div>
-      <div class="col-left"></div>
-      <div class="col-left"></div>
+      <div class="col-left">
+        <div class="footNav">
+          <h2>Dc Comics</h2>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+        </div>
+        <div class="footNav">
+          <h2>Shop</h2>
+          <ul>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-left">
+        <div class="footNav">
+          <h2>Dc</h2>
+          <ul>
+            <li><a href="#">Terms of use</a></li>
+            <li><a href="#">Privacy policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-left">
+        <div class="footNav">
+          <h2>Sites</h2>
+          <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+        </div>
+      </div>
       <div class="col-right">
         <img src="../assets/img/dc-logo-bg.png" alt="Dc Logo" />
       </div>
@@ -22,6 +70,10 @@ export default {};
   background-size: cover;
   .container {
     display: flex;
+    h2 {
+      color: white;
+      padding: 1.875rem 0 1.25rem 0;
+    }
   }
   .col-left {
     width: calc(50% / 3);
